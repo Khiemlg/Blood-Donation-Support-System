e97513e984d5ccd9fe4d276d224e97eca7c69932
duyncse184358 <duyncse184358@fpt.edu.vn>
2025-05-31 23:34:28 +0700

    create donation request . list donationRequest

--- Blood_Donation_System/Presentation/Controllers/DonationController.cs
+++ Blood_Donation_System/Presentation/Controllers/DonationController.cs
@@ -1,4 +1,5 @@
-﻿using Blood_Donation_System.DataAccess;
+﻿using Blood_Donation_System.BusinessLogic.MyModels;
+using Blood_Donation_System.DataAccess;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NETCore.MailKit.Core;
@@ -22,19 +23,40 @@ namespace Blood_Donation_System.Presentation.Controllers
         {
             return View();
         }
-       
+
         [HttpGet]
-        [Route ("DonationRequest/List")]
+        [Route("DonationRequest/List")]
         public async Task<ActionResult> Read()
         {
-            return Ok( new { data = await connect.DonationRequests.ToListAsync() } );
+            return Ok(new { data = await connect.DonationRequests.ToListAsync() });
         }
 
         [HttpPost]
         [Route("DonationRequest/RegisterDonation")]
 
-      // mới sủa lại
+        public async Task<ActionResult> RegisterDonation(String id ,String userid, int typeBlood, int componentBlood, DateOnly PreferredDate, String slot)
+        {
+            var User = await connect.Users.FirstOrDefaultAsync(x => x.UserId.Equals(userid));
+            if(User == null)
+            {
+                return BadRequest("user not found");
+            }
+
+            DonationRequest donation = new DonationRequest();
+            string uniqueSuffix = Guid.NewGuid().ToString("N").Substring(0, 8).ToUpper(); // Ví dụ: "D2A1C3B4"
+
+            donation.RequestId = "BR_" + uniqueSuffix;
+            donation.DonorUserId = userid;
+            donation.BloodTypeId = typeBlood;
+            donation.ComponentId = componentBlood;
+            donation.PreferredDate = PreferredDate;
+            donation.PreferredTimeSlot = slot;
+            donation.Status = "Pending";
+            connect.DonationRequests.Add(donation);
+            return Ok(new { data = donation });
+        }
 
 
+    
     }
 }
