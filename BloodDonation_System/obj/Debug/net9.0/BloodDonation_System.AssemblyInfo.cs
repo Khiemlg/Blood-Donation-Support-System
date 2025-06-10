287f0beda402974caf7c37e991d89d683fe9fa6a
longvb221 <longvbse184332@fpt.edu.vn>
2025-06-10 18:35:25 +0700

    .

--- BloodDonation_System/obj/Debug/net9.0/BloodDonation_System.AssemblyInfo.cs
+++ BloodDonation_System/obj/Debug/net9.0/BloodDonation_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodDonation_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+100813a9434edc14956fc5657b37a216a687ff52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c329a54430fb0a80ebea229bb5fb11931649dce")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodDonation_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodDonation_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
